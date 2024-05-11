f8f791ea6615031c1809734511c70069b19db135
Tofey <105518597+kazmabbas@users.noreply.github.com>
2024-05-11 12:49:13 +0300

    Update ELHYBA.py

--- ELHYBA.py
+++ ELHYBA.py
@@ -1,37 +1,41 @@
 from mody import Mody
-import telebot , requests , json ; from telebot import types
-API_TOKEN = Mody.ELHYBA
-bot = telebot.TeleBot(API_TOKEN)#input('- Enter Token : '))
-RDTHON = ''#معرف قناتك داخل الاقواس
-@bot.message_handler(commands=['start'])
+import requests , telebot
+from telebot import types
+
+bot = Mody.ELHYBA
+bot = telebot.TeleBot(bot)
+
+sh_btn = types.InlineKeyboardButton(text='تحميل', callback_data='s1')
+
+@bot.message_handler(commands=["start"])
 def start(message):
-	id  = message.from_user.id
-	url = requests.get(f"https://api.telegram.org/bot{token}/getchatmember?chat_id={RDTHON}&user_id={id}").text
-	if "member" in url or "creator" in url or "administartor" in url:
-		start = types.InlineKeyboardButton(text='- تنزيل فيديو',callback_data='start')
-		Ronaldo = types.InlineKeyboardMarkup(row_width=2) ; Ronaldo.add(start)
-		bot.send_message(message.chat.id,text='- اهلا بك عزيزي في بوت تحميل من تيك توك اختر من الازرار التاليه',reply_markup=Ronaldo)
-	else:
-		bot.send_message(message.chat.id,'''🚸| عذرا عزيزي
-🔰| عليك الاشتراك بقناة البوت لتتمكن من استخدامه
+    
+    b = types.InlineKeyboardMarkup()
+    b.row_width = 2
+    b.add(sh_btn)
+    
+    bot.send_message(message.chat.id,f"""
+    *مرحبا بك {message.from_user.first_name} في بوت تحميل من تيك توك يحمل فيديو وصوت 💿*""",parse_mode='markdown',reply_markup=b)
 
-- {}
-
-‼️| اشترك ثم ارسل /start'''.format(RDTHON))
-@bot.callback_query_handler(func=lambda call:True)
-def start2(call):
-	if call.data=='start':
-		ji = bot.send_message(call.message.chat.id,text='- اهلاً بَك عزيزي ارسل الرابط الان .')
-		bot.register_next_step_handler(ji,dow)
-def dow(message):
-		url = message.text
-		headers = {"referer":"https://lovetik.com/sa/video/","origin":"https://lovetik.com","user-agent":"Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"}
-		payload = {"query":url}
-		url =requests.post("https://lovetik.com/api/ajax/search",headers = headers,data=payload).json()
-		try:
-			respones=json.loads('{"ok":"true","Video":{"authorUser":"'+url['author']+'","authorName":"'+url['author_name']+'","authorImage":"'+url['author_a']+'","cover":"'+url['cover']+'","vidID":"'+url['vid']+'","desc":"'+url["desc"]+'","link":"'+url['links'][4]['a']+'","audioName":"'+url['links'][5]['s']+'","audioLink":"'+url['links'][5]['a']+'"}}') 
-		except:
-			bot.send_message(message.chat.id,'- عذراً عزيزي الرابط غير صالح !')
-		bot.send_video(message.chat.id,respones['Video']['link'],caption='- Done Download Video .')
-if __name__=="__main__":
-	bot.infinity_polling()
+@bot.callback_query_handler(func=lambda call: True)
+def sh(call):
+  if call.data=='s1':
+   bot.send_message(call.message.chat.id,'- ارسل الرابط!')
+   @bot.message_handler(func=lambda m: True)
+   def Url(message):
+    bot.send_message(message.chat.id,"<strong>جاري التحميل انتظر قليلا ...</strong>",parse_mode="html")
+    msg = message.text
+    try:
+     url = requests.get(f'https://dev-broksuper.pantheonsite.io/api/e/mp3.php?url={msg}').json()
+     a = url["video"]["videoURL"]
+     b = url['audioURL']
+     
+     bot.send_video(message.chat.id,a,caption='تم تحميل بواسطة @RDthon')
+     bot.send_voice(message.chat.id,b,caption='تم تحميل بواسطة @RDthon')
+    
+    
+    except:
+     bot.send_message(message.chat.id,"تأكد من الرابط..!")
+  
+print('run')
+bot.infinity_polling()
